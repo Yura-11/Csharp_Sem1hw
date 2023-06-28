ca85e0757e62acdeffb507502f3bc075330aea61
Yura <romanov.e180@gmail.com>
2023-06-28 20:06:06 +0300

    task5

---
+++ HW2/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if(numb < 1000 && numb > 100)
+{
+    System.Console.WriteLine($"Вторая цифра  {numb} является {numb / 10 % 10} ");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число"); 
+}
+
+
