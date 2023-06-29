c19627ab23d2b0a07c4436697049c94118fbca42
Yura <romanov.e180@gmail.com>
2023-06-29 18:05:49 +0300

    HWTask1

---
+++ HW3/Task1Pall/Program.cs
@@ -0,0 +1,19 @@
+﻿// Проверить явлеется ли пятизначное число полиндромом"
+System.Console.WriteLine("Введите 5значное число ");
+string numb = Console.ReadLine();
+int a = numb.Length;
+if( a == 5)
+{
+    if(numb[0] == numb[4] && numb[1] == numb[3])
+    {
+        System.Console.WriteLine("Число является палиндромом");
+    }
+    else 
+    {
+        System.Console.WriteLine("Число не является палиндромом");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено не 5значное число");
+}
