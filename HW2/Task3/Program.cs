bc9aab70d1acb797eb87b8faf343873728177950
Yura <romanov.e180@gmail.com>
2023-06-29 10:37:33 +0300

    Task7

---
+++ HW2/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число дня недели:");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb<5 && numb >= 1)
+{
+    Console.WriteLine("нет");
+}
+else if (numb>5 && numb<=7)
+{
+  Console.WriteLine("да");  
+}
+else
+{
+    Console.WriteLine("Это не день недели");
+}
+
+    
+     
