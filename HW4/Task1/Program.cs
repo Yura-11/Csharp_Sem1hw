82d800f33ac808a6191da985c050fd53235e0ca7
Yura <romanov.e180@gmail.com>
2023-07-10 11:22:21 +0300

    HW4T1

---
+++ HW4/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int numb1 = ReadInt("Введите первое число : ");
+int numb2 = ReadInt("Введите второе число : ");
+Degree(numb1, numb2);
+
+void Degree(int numb1, int numb2)
+{
+    int result = 1;
+    for (int i = 1; i <= numb2; i++)
+    {
+        result = result * numb1;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
