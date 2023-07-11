6150fd7c055438dac571a19cbfb7b45fa181eb3f
Yura <romanov.e180@gmail.com>
2023-07-11 13:02:15 +0300

    HW5T2

---
+++ HW5/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int size = 4;
+int[] numb = new int[size];
+FillArrayRandomNumbers(numb);
+PrintArray(numb);
+
+int sumNumbersEvenIndex = 0;                        
+for (int i = 1; i < numb.Length; i += 1)
+{
+    sumNumbersEvenIndex += numb[i];
+}
+Console.Write(sumNumbersEvenIndex);
+
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
